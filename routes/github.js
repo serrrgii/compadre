4d9c1f4813d79f446cd300ed32dd5cd87c2756e9
Sergi Borras <sergi.borras@LT0184.local>
2014-10-20 18:41:25 +0200

    log action and labels

--- routes/github.js
+++ routes/github.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/webhooks', function(req, res) {
-  console.log(req.body);
+  console.log("issue action: "+req.body.action+"\n"+
+	  "labels: "+JSON.stringify(req.body.issue.labels));
 });
 
 module.exports = router;
