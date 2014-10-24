7772ef03457e4b038196b270ca21abda932dccf7
Sergi Borras <sergi.borras@LT0184.local>
2014-10-24 15:56:19 +0200

    push validated labels and save issue

--- models/issue.js
+++ models/issue.js
@@ -8,34 +8,53 @@ var issueSchema = new Schema({
   issueId: Number,
   labels : [{ name: String, date: Date }]
 });
-
 issueSchema.methods.validateLabel = function(label) {
-  console.log("validate label: "+label);
   if (_.contains(VALID_LABELS, label) === false) {
 	  return false;
   }
   var labelIndex = _.indexOf(VALID_LABELS, label);
-  var otherLabels = _.without(VALID_LABELS, label);
-  var splittedLabels = _.partition(otherLabels, function(element) {
-    var elementIndex = _.indexOf(otherLabels, element);
+  var splittedLabels = _.partition(VALID_LABELS, function(element) {
+
+    var elementIndex = _.indexOf(VALID_LABELS, element);
     return elementIndex < labelIndex;
   });
-  console.log("splitted labels first: "+_.first(splittedLabels));  
+
+  var mandatoryLabels = _.first(splittedLabels);
+  var bannedLabels = _.last(splittedLabels);
+  var currentLabels = _.pluck(this.labels, 'name');
+  
+  return validateMandatoryLabels(mandatoryLabels, currentLabels) && 
+	  validateBannedLabels(bannedLabels, currentLabels);
 }
 
 issueSchema.methods.processRemoteLabels = function(labels) {
   var filtered = filterRemoteLabels(labels);
-  if (this.validateLabel(_.last(filtered))) {
+  var label = _.last(filtered);
+  if (this.validateLabel(label)) {
+    this.labels.push({ name: label, date: new Date()});
+    this.save(function(error) {
+      if (error) {
+        return console.log("error saving issue: "+error);
+      }
+      console.log("saved issue: "+this);
+    }); 
   }
-}
-
-issueSchema.statics.filterRemoteLabels = function(labels) {
-  return filterRemoteLabels(labels);
+  else {
+    console.log("label: "+label+", validation failed");
+  }
 }
 
 var filterRemoteLabels = function(labels) {
   return _.intersection(VALID_LABELS, 
 	_.pluck(labels, 'name'));
+}
+
+var validateMandatoryLabels = function(mandatoryLabels, currentLabels) {
+  return _.intersection(mandatoryLabels, currentLabels).length == mandatoryLabels.length;
+}
+
+var validateBannedLabels = function(bannedLabels, currentLabels) {
+  return _.intersection(bannedLabels, currentLabels).length == 0;	
 }
 
 module.exports = mongoose.model('Issue', issueSchema);
--- routes/github.js
+++ routes/github.js
@@ -2,33 +2,30 @@ var express = require('express');
 var router = express.Router();
 var Issue = require('../models/issue.js');
 
+var LABELED_ACTION = "labeled";
+
 router.post('/webhooks', function(req, res) {
   
-  var labels = req.body.issue.labels;
-  var filteredLabels = Issue.filterRemoteLabels(labels);
+  if (req.body.action !== LABELED_ACTION) {
+    return res.send(422, "Unable to process this action");
+  }  
 
-  console.log("issue action: "+req.body.action+"\n"+
-    "labels: "+JSON.stringify(labels)+"\n"+
-    "filtered labels: "+filteredLabels.length);
-  
+  var labels = req.body.issue.labels;
   var githubIssue = req.body.issue;
 
   Issue.findOne({ issueId : githubIssue.id }, function (err, issue) {
     if (err) {
-      res.send(500, "Database error");
+      return res.send(500, "Database error");
     }
     if (issue) {
-      console.log("issue found: "+issue+"\n");
-
       issue.processRemoteLabels(labels);
     }
     else {
-      console.log("did not find issue with id: "+githubIssue.id);
       Issue.create({ issueId: githubIssue.id }, function (err, issue) {
         if (err) {
-	  res.send(500, "Database error");
+	  return res.send(500, "Database error");
 	}
-	console.log("created issue "+issue);
+	issue.processRemoteLabels(labels);
       }); 
     }
   });
