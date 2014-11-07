cdc55ac169171a1a0945bf145d907366263442e4
Sergi Borras <sergi.borras@LT0184.local>
2014-11-07 16:28:18 +0100

    validate insert and remove label

--- models/issue.js
+++ models/issue.js
@@ -4,39 +4,39 @@ var mongoose = require('mongoose'),
 
 var VALID_LABELS = ["next", "development", "test", "ready"];
 var REVERTIBLE = ["test"];
-var VALID_ACTIONS = ["labeled", "unlabeled"];
+var ACTIONS = { LABELED: "labeled", 
+	  UNLABELED: "unlabeled"};
 
 var issueSchema = new Schema({
   issueId: Number,
   labels : [{ name: String, date: Date }]
+})
 
 issueSchema.statics.validateAction = function(action) {
-  return _.contains(VALID_ACTIONS, action);	
+  return _.contains(_.values(VALID_ACTIONS), action);	
 }
 
-issueSchema.methods.validateLabel = function(label) {
+issueSchema.methods.validateLabel = function(label, action) {
   if (_.contains(VALID_LABELS, label) === false) {
 	  return false;
   }
-  var labelIndex = _.indexOf(VALID_LABELS, label);
-  var splittedLabels = _.partition(VALID_LABELS, function(element) {
 
-    var elementIndex = _.indexOf(VALID_LABELS, element);
-    return elementIndex < labelIndex;
-  });
-
-  var mandatoryLabels = _.first(splittedLabels);
-  var bannedLabels = _.last(splittedLabels);
   var currentLabels = _.pluck(this.labels, 'name');
-  
-  return validateMandatoryLabels(mandatoryLabels, currentLabels) && 
-	  validateBannedLabels(bannedLabels, currentLabels);
+
+  if(action === ACTIONS.LABELED) {
+    return validateInsertLabel(label, currentLabels);
+  }
+  else if (action === ACTIONS.UNLABELED) {
+    return validateRemoveLabel(label, currentLabels);	  
+  }
+
+  return false;
 }
 
-issueSchema.methods.processRemoteLabels = function(labels) {
+issueSchema.methods.processRemoteLabels = function(labels, action) {
   var filtered = filterRemoteLabels(labels);
   var label = _.last(filtered);
-  if (this.validateLabel(label)) {
+  if (this.validateLabel(label, action)) {
     this.labels.push({ name: label, date: new Date()});
     this.save(function(error) {
       if (error) {
@@ -55,6 +55,30 @@ var filterRemoteLabels = function(labels) {
 	_.pluck(labels, 'name'));
 }
 
+var validateInsertLabel = function(labels, currentLabels) {
+  var labelIndex = _.indexOf(VALID_LABELS, label);
+  var splittedLabels = _.partition(VALID_LABELS, function(element) {
+
+    var elementIndex = _.indexOf(VALID_LABELS, element);
+    return elementIndex < labelIndex;
+  });
+  var mandatoryLabels = _.first(splittedLabels);
+  var bannedLabels = _.last(splittedLabels);
+  
+  return validateMandatoryLabels(mandatoryLabels, currentLabels) && 
+	  validateBannedLabels(bannedLabels, currentLabels);
+}
+var validateRemoveLabel = function(label, currentLabels) {
+  if (_.contains(REVERTIBLE, _.last(currentLabels)) === false) {
+    return false;
+  }	  
+  var index = currentLabels.length-1;
+  if (index === 0) {
+    return true;
+  } else {
+    return label === VALID_LABELS[index-1];
+  }
+}
 var validateMandatoryLabels = function(mandatoryLabels, currentLabels) {
   return _.intersection(mandatoryLabels, currentLabels).length == mandatoryLabels.length;
 }
